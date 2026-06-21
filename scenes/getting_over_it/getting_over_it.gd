4f364ef235e0d4a36c78eb8f1726a9e6feff88f4
wailer <andrypirk@gmail.com>
2026-06-21 12:31:49 +0300

    shiza is over

--- scenes/getting_over_it/getting_over_it.gd
+++ scenes/getting_over_it/getting_over_it.gd
@@ -8,25 +8,30 @@ extends Node2D
 # @onready var joint: DampedSpringJoint2D = $Joint
 
 
+func _ready() -> void:
+	player.add_collision_exception_with(flail)
+
+
 func _physics_process(delta: float) -> void:
 	flail.freeze = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-	if flail.freeze:
-		flail.set_axis_velocity(Vector2.ZERO)
 	player.freeze = Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
-	if player.freeze:
-		player.set_axis_velocity(Vector2.ZERO)
 
 	if player.freeze and not flail.freeze:
+		player.linear_velocity = Vector2.ZERO
 		flail.apply_central_force((get_global_mouse_position() - flail.global_position).normalized() * force_f)
 	elif not player.freeze and flail.freeze:
+		flail.linear_velocity = Vector2.ZERO
 		var want_player := 2 * flail.global_position - get_global_mouse_position()
+		want_player = get_global_mouse_position()
+		# want_player = get_global_mouse_position()
 		player.apply_central_force((want_player - player.global_position).normalized() * force_p)
 	elif not player.freeze and not flail.freeze:
-		flail.apply_central_force((get_global_mouse_position() - flail.global_position).normalized() * force_f)
-
-		var center := (flail.global_position + player.global_position) * 0.5
-		var want_player := 2 * center - get_global_mouse_position()
-		player.apply_central_force((want_player - player.global_position).normalized() * force_p)
+		pass
+		# flail.apply_central_force((get_global_mouse_position() - flail.global_position).normalized() * force_f)
+		#
+		# var center := (flail.global_position + player.global_position) * 0.5
+		# var want_player := 2 * center - get_global_mouse_position()
+		# player.apply_central_force((want_player - player.global_position).normalized() * force_p)
 
 	_apply_constaint()
 
@@ -42,32 +47,55 @@ func _draw() -> void:
 		draw_dashed_line(to_local(get_global_mouse_position()), to_local(flail.global_position), Color.RED)
 	elif not player.freeze and flail.freeze:
 		var want_player := 2 * flail.global_position - get_global_mouse_position()
+		want_player = get_global_mouse_position()
 		player.apply_central_force((want_player - player.global_position).normalized() * force_p)
+		# want_player = get_global_mouse_position()
 		draw_dashed_line(to_local(want_player), to_local(player.global_position), Color.RED)
 	elif not player.freeze and not flail.freeze:
-		var center := (flail.global_position + player.global_position) * 0.5
-		var want_player := 2 * center - get_global_mouse_position()
-		draw_dashed_line(to_local(get_global_mouse_position()), to_local(flail.global_position), Color.RED)
-		draw_dashed_line(to_local(want_player), to_local(player.global_position), Color.RED)
+		pass
+		# var center := (flail.global_position + player.global_position) * 0.5
+		# var want_player := 2 * center - get_global_mouse_position()
+		# draw_dashed_line(to_local(get_global_mouse_position()), to_local(flail.global_position), Color.RED)
+		# draw_dashed_line(to_local(want_player), to_local(player.global_position), Color.RED)
+
+
+	# draw_line(to_local(player.global_position), to_local(player.global_position + player.linear_velocity), Color.LIME)
+	# draw_line(to_local(flail.global_position), to_local(flail.global_position + flail.linear_velocity), Color.LIME)
 
 
 func _apply_constaint() -> void:
-	const MAX_DIST: float = 50
+	const MAX_DIST := 50.0
 
-	var dist := player.global_position.distance_to(flail.global_position)
-	dist -= MAX_DIST
+	var delta := flail.global_position - player.global_position
+	var delta_len := delta.length()
 
-	if dist < 0:
+	if delta_len <= MAX_DIST:
 		return
 
+	var dir := delta / delta_len
+	var error := delta_len - MAX_DIST
 
-	var p_to_f := player.global_position.direction_to(flail.global_position)
+	var p_bias: float = 0.5
 
-	player.global_position += p_to_f * dist * 0.5
-	flail.global_position += -p_to_f * dist * 0.5
+	if player.freeze and not flail.freeze:
+		p_bias = 0
+	elif not player.freeze and flail.freeze:
+		p_bias = 1
 
-	flail.linear_velocity -= maxf(_projection(flail.linear_velocity, p_to_f), 0) * p_to_f
-	player.linear_velocity -= maxf(_projection(player.linear_velocity, -p_to_f), 0) * -p_to_f
+	player.global_position += dir * error * p_bias
+	flail.global_position -= dir * error * (1 - p_bias)
+
+	var rel_vel := (flail.linear_velocity - player.linear_velocity).dot(dir)
+	if rel_vel <= 0:
+		return
+
+	var inv_mass_sum := 1.0 / player.mass + 1.0 / flail.mass
+
+	var impulse_mag := -rel_vel / inv_mass_sum
+	var impulse := dir * impulse_mag
+
+	player.apply_impulse(-impulse * p_bias)
+	flail.apply_impulse(impulse * (1 - p_bias))
 
 
 func _projection(of: Vector2, onto: Vector2) -> float:
