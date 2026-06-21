828cfdf34c87da43dfd4787163ffc1e0309b85e2
wailer <andrypirk@gmail.com>
2026-06-21 11:20:32 +0300

    variant 1

--- scenes/getting_over_it/getting_over_it.gd
+++ scenes/getting_over_it/getting_over_it.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 @onready var flail: RigidBody2D = $Flail
 @onready var player: RigidBody2D = $Player
-@onready var joint: DampedSpringJoint2D = $Joint
+# @onready var joint: DampedSpringJoint2D = $Joint
 
 
 func _physics_process(delta: float) -> void:
@@ -28,6 +28,8 @@ func _physics_process(delta: float) -> void:
 		var want_player := 2 * center - get_global_mouse_position()
 		player.apply_central_force((want_player - player.global_position).normalized() * force_p)
 
+	_apply_constaint()
+
 
 func _process(delta: float) -> void:
 	queue_redraw()
@@ -47,3 +49,26 @@ func _draw() -> void:
 		var want_player := 2 * center - get_global_mouse_position()
 		draw_dashed_line(to_local(get_global_mouse_position()), to_local(flail.global_position), Color.RED)
 		draw_dashed_line(to_local(want_player), to_local(player.global_position), Color.RED)
+
+
+func _apply_constaint() -> void:
+	const MAX_DIST: float = 50
+
+	var dist := player.global_position.distance_to(flail.global_position)
+	dist -= MAX_DIST
+
+	if dist < 0:
+		return
+
+
+	var p_to_f := player.global_position.direction_to(flail.global_position)
+
+	player.global_position += p_to_f * dist * 0.5
+	flail.global_position += -p_to_f * dist * 0.5
+
+	flail.linear_velocity -= maxf(_projection(flail.linear_velocity, p_to_f), 0) * p_to_f
+	player.linear_velocity -= maxf(_projection(player.linear_velocity, -p_to_f), 0) * -p_to_f
+
+
+func _projection(of: Vector2, onto: Vector2) -> float:
+	return of.dot(onto) / onto.length()
