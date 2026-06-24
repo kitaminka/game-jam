5373adb65f0df1d30627c8ca1bcfe12427678b09
zhizhoid <misha2006.mk@gmail.com>
2026-06-24 16:32:06 +0300

    added variable_tilemap

---
+++ scenes/tiles/variable_tilemap.gd
@@ -0,0 +1,12 @@
+extends TileMapLayer
+
+
+@export var custom_textures: Dictionary[int, Texture2D]
+
+
+func _ready() -> void:
+    for src_id: int in custom_textures.keys():
+        var texture := custom_textures[src_id]
+        print("%s: swapping source %s texture to %s" % [name, src_id, texture.resource_name])
+        var src := tile_set.get_source(src_id) as TileSetAtlasSource
+        src.texture = texture
