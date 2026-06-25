a777d9bd408ce39c326a644068dcd5c5ec798657
wailer <andrypirk@gmail.com>
2026-06-25 20:11:06 +0300

    chase level

--- scenes/levels/level_chase/tanks.gd
+++ scenes/levels/level_chase/tanks.gd
@@ -4,12 +4,16 @@ extends PathFollow2D
 
 var _started: bool = false
 @onready var cnb: ChainAndBalls = ChainAndBalls.get_instance()
-
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	($Death as Area2D).body_entered.connect(_kill_object)
 	($Death as Area2D).area_entered.connect(_kill_object)
+
+	($TankDeath as Area2D).area_entered.connect(_kill_tank)
+	($TankDeath as Area2D).body_entered.connect(_kill_tank)
+
+	animation_player.speed_scale = randf_range(0.9, 1.1)
 
 
 func _physics_process(delta: float) -> void:
@@ -18,9 +22,11 @@ func _physics_process(delta: float) -> void:
 
 	for child: Node in get_children():
 		if child.is_in_group(&"upright"):
-			var s := child as Sprite2D
+			var s := child as Node2D
 			s.global_rotation = 0
-			s.flip_h = Vector2.RIGHT.rotated(rotation).x < 0
+			var x := Vector2.RIGHT.rotated(rotation).x
+			if not is_zero_approx(x):
+				s.scale.x = signf(x)
 
 	progress += delta * speed
 
@@ -41,3 +47,10 @@ func _kill_object(node: Node) -> void:
 
 func start() -> void:
 	_started = true
+
+
+
+func _kill_tank(_v: Variant) -> void:
+	speed = 0
+	animation_player.play("death")
+	$Death.queue_free()
