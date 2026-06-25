215e292f3df0b9e4e3c47ab5660cdf7c043845ee
wailer <andrypirk@gmail.com>
2026-06-25 15:46:02 +0300

    fix exit

--- scenes/exit/exit.gd
+++ scenes/exit/exit.gd
@@ -6,9 +6,12 @@ signal player_exited
 
 @export var win_condition_enemies: bool = false
 var _enemies_left: int = 0
+@onready var label: Label = $Label
 
 
 func _ready() -> void:
+	remove_child(label)
+
 	if win_condition_enemies:
 		for enemy in get_tree().get_nodes_in_group(&"enemy"):
 			enemy.got_lobotomized.connect(_on_enemy_death, CONNECT_ONE_SHOT)
@@ -25,9 +28,7 @@ func _on_enemy_death() -> void:
 	_enemies_left -= 1
 	_enemies_left = maxi(_enemies_left, 0)
 
-	if _enemies_left <= 0:
-		_update_visual()
-
+	_update_visual()
 	_check_player()
 
 
@@ -37,6 +38,18 @@ func _update_visual() -> void:
 
 
 func _check_player() -> void:
+	if _enemies_left >= 0:
+		var inst: Label = label.duplicate()
+		add_child(inst)
+
+		inst.text = "%d left" % _enemies_left
+
+		var t := inst.create_tween().set_parallel()
+		t.tween_property(inst, "position:y", -20, 2.0).as_relative().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+		t.tween_property(inst, "modulate:a", 0.0, 1).set_delay(1)
+		t.chain().tween_callback(inst.queue_free)
+
+
 	if _enemies_left <= 0 and (has_overlapping_bodies() or has_overlapping_areas()):
 		print("Plater exited!")
 		player_exited.emit()
